81499d9f2bbdbfb738da2d0792bbdaad73300ab5
dzanis <zanisdukalskis@gmail.com>
2017-12-14 09:45:48 +0300

    first commit

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1513205764,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/playertank-sheet0.png",
+		"images/shell-sheet0.png",
+		"images/tiledbackground.png",
+		"images/enemytank-sheet0.png",
+		"images/tilemap.png",
+		"images/tilemap2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
